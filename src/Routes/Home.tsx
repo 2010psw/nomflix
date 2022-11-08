2f899dcbcdeaad63a7e9e0971a5657aea137a1df
psw <2010psw@naver.com>
2022-11-08 11:53:53 +0900

    9.9 Box Animations part One

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -54,8 +54,13 @@ const Box = styled(motion.div)<{ bgPhoto: string }>`
   background-size: cover;
   background-position: center center;
   height: 200px;
-
-  font-size: 66px; ;
+  font-size: 66px;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
 `;
 
 const rowVariants = {
@@ -67,6 +72,21 @@ const rowVariants = {
   },
   exit: {
     x: -window.outerWidth - 190,
+  },
+};
+
+const boxVariants = {
+  normal: {
+    scale: 1,
+  },
+  hover: {
+    scale: 1.3,
+    y: -50,
+    transition: {
+      delay: 0.5,
+      duration: 0.3,
+      type: "tween",
+    },
   },
 };
 
@@ -120,6 +140,10 @@ function Home() {
                   .map((movie) => (
                     <Box
                       key={movie.id}
+                      whileHover="hover"
+                      initial="normal"
+                      variants={boxVariants}
+                      transition={{ type: "tween" }}
                       bgPhoto={makeImagePath(movie.backdrop_path, "w400")}
                     ></Box>
                   ))}
